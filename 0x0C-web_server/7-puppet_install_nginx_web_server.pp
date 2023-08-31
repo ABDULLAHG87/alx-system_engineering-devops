81655616d5e41526022703d42e436e734a000906
Abdulhakeem Abdullahi <hakeemabdullah87@gmail.com>
2023-08-30 18:04:48 -0700

    created task 5 install nginx webserver with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,41 @@
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => "
+server {
+    listen      80 default_server;
+    listen      [::]:80 default_server;
+    root        /etc/nginx/html;
+    index       index.html index.htm;
+
+    location / {
+        return 200 'Hello World!';
+    }
+
+    location /redirect_me {
+        return 301 'https://www.example.com';
+    }
+}
+",
+  require => Package['nginx'],
+}
+
+exec { 'nginx_reload':
+  command     => '/usr/sbin/service nginx reload',
+  refreshonly => true,
+  require     => File['/etc/nginx/sites-available/default'],
+}
+
+exec { 'nginx_test_config':
+  command => '/usr/sbin/nginx -t',
+  require => Exec['nginx_reload'],
+}
+
+Service['nginx'] ~> Exec['nginx_reload']
