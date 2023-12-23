d455fb9c598e0a2325bfeaa6be8402478c77d138
root <hakeemabdullah87@gmail.com>
2023-12-23 12:40:36 -0800

    REFACTOR: add package python3-pip in the configuration file 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# install a package with puppet
-
-package { 'python3-pip':
-  ensure  => present,
-}
-# Install flask version 2.1.0
-
+# install a package with pupet
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
