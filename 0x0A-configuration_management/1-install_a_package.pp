6a617f6a0eb71311612f553fa726e35b900a7bd9
root <hakeemabdullah87@gmail.com>
2023-12-23 02:41:28 -0800

    REFACTOR: change code to pass the checker

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # install a package with puppet
 
-package{ 'flask':
+# Ensure 'pip3' is installed
+package { 'python3-pip:
+  ensure  => present,
+}
+
+# Install flask version 2.1.0
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-}
+  require  => Package['python3-pip'],
+  }
