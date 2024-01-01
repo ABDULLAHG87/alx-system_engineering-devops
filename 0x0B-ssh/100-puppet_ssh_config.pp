75ea2515d0d2f3390af1339e9e3e78f74b47d52f
root <hakeemabdullah87@gmail.com>
2024-01-01 07:03:34 -0800

    BUGFIX: fix the code to pass the checker

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 # using puppet to make connection without password
 
-# Define a class for configuring SSH client
-class ssh_client_config {
-
-# Install the 'openssh-client' package (if not already installed)
-package { 'openssh-client':
-  ensure => installed,
-}
-
 # Create the SSH configuration file
 file { '/etc/ssh/ssh_config':
   ensure => present,
@@ -19,7 +11,6 @@ file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^#?PasswordAuthentication',
-  notify => Service['ssh'],
 }
 
 # Declare identity file
@@ -27,7 +18,6 @@ file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^#?IdentityFile',
-  notify => Service['ssh'],
 }
 
 # Ensure the SSH service is running
