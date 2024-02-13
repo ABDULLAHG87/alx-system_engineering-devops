9201d5f3e76cfa938d5d37487e9647899120ee15
root <hakeemabdullah87@gmail.com>
2024-02-13 15:42:51 -0800

    FEAT: add the py file 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script that displays number of subscribers for a given subreddit
+"""
+
+from request import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the reddit API and returns number of subscribers
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent - {'User-agent': 'Google Chrome version 121.0.6167.161'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+
+    try:
+        return results.get('data').get('subscribers')
+    except Exception:
+        return 0
