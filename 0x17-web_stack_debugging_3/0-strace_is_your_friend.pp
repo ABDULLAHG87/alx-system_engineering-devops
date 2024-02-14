7fce0cb648f1603c5d3a610da5fefe553f9527fe
root <hakeemabdullah87@gmail.com>
2024-02-14 13:43:34 -0800

    FEAT: add puppet file 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet file that fixes bad `phpp`extensions to `php` in wordpress
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
