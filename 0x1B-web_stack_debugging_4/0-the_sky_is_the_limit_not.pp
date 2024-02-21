1975bb8e8f41efc9806cb1aa6beded23fc83b8f9
root <hakeemabdullah87@gmail.com>
2024-02-21 03:25:01 -0800

    BUGFIX: fix error in the puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
+# increase the ulimit of the default file
 exec {'fix--for-nginx':
   command => 'sed -i "s/15/4096" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'}
-  }
+  } ->
 
 # Restarting Nginx
--> exec { 'nginx-restart':
+exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'}
   }
